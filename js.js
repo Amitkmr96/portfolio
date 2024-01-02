d67a19b35d133564ef971d294e879896e1e94721
Amit <amitkmr9911@gmail.com>
2024-01-02 19:27:26 +0530

    All updated  changes

--- js.js
+++ js.js
@@ -1,38 +1,45 @@
 $(".line").each(function () {
-    $(this).css(
-      {
-        width: $(this).attr("data-progress") + "%",
-      }
-      
-    );
-    
+  $(this).css({
+    width: $(this).attr("data-progress") + "%",
   });
+});
 
- 
- 
-    $(".view-btn").click(function(){
-      $(".hide-row").slideToggle("slow");
-      
-    });
- 
+$(".view-btn").click(function () {
+  $(".hide-row").slideToggle("slow");
+});
 
-
- 
-  $(document).ready(function(){
-    $("#flip").click(function(){
-      $("#mnav").slideToggle("slow");
-      if($("#flip").hasClass("fa-bars"))
-      {
-        $("#flip").removeClass("fa-bars");
-        $("#flip").addClass("fa-times");
-      }
-      else
-      {
-        $("#flip").addClass("fa-bars");
-        $("#flip").removeClass("fa-times");
-      }
-      
-    });
+$(document).ready(function () {
+  $("#flip").click(function () {
+    $("#mnav").slideToggle("slow");
+    if ($("#flip").hasClass("fa-bars")) {
+      $("#flip").removeClass("fa-bars");
+      $("#flip").addClass("fa-times");
+    } else {
+      $("#flip").addClass("fa-bars");
+      $("#flip").removeClass("fa-times");
+    }
   });
+});
 
- 
+$(document).ready(function () {
+  // Hide all tab content except the first one
+  $(".tab-content:not(:first)").hide();
+
+  // Add 'active' class to the first tab link
+  $(".tab-links li:first").addClass("active");
+
+  // Click function for tab links
+  $(".tab-links a").on("click", function (e) {
+    var currentAttrValue = $(this).attr("href");
+
+    // Show/Hide tabs
+    $(".tab-content").hide();
+    $(currentAttrValue).show();
+
+    // Change active tab link
+    $(".tab-links li").removeClass("active");
+    $(this).parent("li").addClass("active");
+
+    e.preventDefault();
+  });
+});
